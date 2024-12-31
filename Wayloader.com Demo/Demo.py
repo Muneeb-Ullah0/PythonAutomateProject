0e179b6bc6cbe42d26943312c4bce62187c4cd45
Muneeb-Ullah0 <Muneebullahkhan922@gmail.com>
2024-12-31 12:42:37 +0500

    variables

---
+++ Wayloader.com Demo/Demo.py
@@ -0,0 +1,94 @@
+# import requests
+#
+# response = requests.get('https://reqres.in/api/users?page=2')
+#
+# if response.status_code == 200:
+#     print("Request Successful!")
+#     print(response.status_code)
+#     print(response.json())
+# else:
+#     response.raise_for_status()
+#     print("Request Failed!")
+
+# X = name , age , address = "Muneeb", 25, "Mumbai"
+# print (X)
+# fruits = ["Apple", "Banana", "Orange"]
+# print(fruits)
+
+# fruits = ["apple", "banana", "cherry"]
+# x = fruits
+# print(x)
+
+
+# age = 25name = "Muneeb"
+# age = 25
+# address = "Mumbai"
+# print(name + str(age) + address)
+
+# Name = "Muneeb"
+# def Myfunc():
+#     print("My Name is" + str(Name))
+# Myfunc()
+#
+# #
+# def class1():
+#     name = "Muneeb"
+#     print (name)
+# class1()
+#
+# name = "Muneeb"
+# age = 25
+# address = "Mumbai"
+# x = name , age , address
+# print(x)
+# name = "Muneeb"
+# def Myclass():
+#    print("My name is " + name)
+# Myclass()
+
+# def Value():
+#     name = "Muneeb"
+#     print(name)
+# Value()
+
+# x = "white"
+# def value():
+#     global x
+#     x = "black"
+#     print (x)
+# value()
+# print(x)
+
+# name = "Muneeb"
+# def Myclass():
+#     print("My name is " + name)
+# Myclass()
+# x = "cricket"
+# def myfunc():
+#   print= ("i am a game player of ")
+# myfunc()
+# print("Python is " + x)
+#Q1
+# greetings = "Hello worid"
+# print(greetings)
+
+# Assign two numbers to variables a and b
+a = 10
+b = 5
+
+# Calculate sum, difference, product, and quotient
+sum_result = a + b
+difference_result = a - b
+product_result = a * b
+quotient_result = a / b
+
+# Print the results
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
+
+
+firstname = "Muneeb "
+lastname = "Ullah"
+print(firstname + lastname)
