f3d5e9766f4ce7ac32bad0935f28e691af19eb68
Muneeb-Ullah0 <Muneebullahkhan922@gmail.com>
2024-12-31 16:56:30 +0500

    variable commit

---
+++ Wayloader.com Demo/Variables.py
@@ -0,0 +1,60 @@
+# greetings = "Hello worid"
+# print(greetings)
+
+# Assign two numbers to variables a and b
+# a = 10
+# b = 5
+
+# Calculate sum, difference, product, and quotient
+# sum_result = a + b
+# difference_result = a - b
+# product_result = a * b
+# quotient_result = a / b
+#
+# # Print the results
+# print("Sum:", sum_result)
+# print("Difference:", difference_result)
+# print("Product:", product_result)
+# print("Quotient:", quotient_result)
+#
+#
+# firstname = "Muneeb "
+# lastname = "Ullah"
+# # print(firstname + lastname)
+#
+# a:int = 5
+# b:int = 10
+#
+# a ,b = a , b
+# print("a =" , a)
+# print("b =",b)
+#
+# # Create a variable count and set it to 0
+# count = 0
+#
+# # Increment by 5, decrement by 2, and multiply by 3
+# count += 5    # Increment by 5
+# count -= 2    # Decrement by 2
+# count *= 3    # Multiply by 3
+#
+# # Print the final value
+# print(count)
+
+# name = "Muneeb"
+# age =24
+# address = "Swabi"
+# print(name,age,address)
+
+# def myclass():
+#     name = "muneeb"
+#     age = 24
+#     address = "swabi"
+#     print(name,age,address)
+# myclass()
+
+name = "muneeb"
+age = 24
+address = "swabi"
+def myfunc():
+    print(name,age,address)
+myfunc()
