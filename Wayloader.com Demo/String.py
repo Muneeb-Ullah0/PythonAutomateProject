6b90582b9441be3172614b97ded7d8bb1687ff1c
Muneeb-Ullah0 <Muneebullahkhan922@gmail.com>
2025-01-07 16:51:41 +0500

    String Commit

---
+++ Wayloader.com Demo/String.py
@@ -0,0 +1,25 @@
+
+# A string is a sequence of characters enclosed in quotes. Strings are used to represent text in Python.
+
+# Example 1: A simple string
+example_1 = "Hello, World!"
+
+# Example 2: A multiline string
+example_2 = """This is
+a multiline
+string."""
+
+# Example 3: A string with special characters
+example_3 = "Special characters: \n (newline), \t (tab), \\ (backslash)"
+
+# Example 4: A raw string
+example_4 = r"This is a raw string: \n (not a newline), \\ (backslash)"
+
+# Example 5: String concatenation
+example_5 = "Hello, " + "World!"
+
+# Example 6: A string using single quotes
+example_6 = 'This is a string in single quotes.'
+
+print(example_1, example_2, example_3, example_4, example_5, example_6)
+
