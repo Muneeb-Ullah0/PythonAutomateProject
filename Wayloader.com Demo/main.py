4e77d1ba368750ba12cd689d29d88c3687fe0140
Muneeb-Ullah0 <Muneebullahkhan922@gmail.com>
2025-01-09 17:49:54 +0500

    main.py changes and screenshot.png

--- Wayloader.com Demo/main.py
+++ Wayloader.com Demo/main.py
@@ -10,7 +10,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 driver.get("https://www.google.com")
-
 search_box = driver.find_element(By.NAME, "q")
 search_box.send_keys("python")
 
