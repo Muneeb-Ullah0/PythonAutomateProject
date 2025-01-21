43990b81809431393d0da1030d84f8931069a84e
Muneeb-Ullah0 <Muneebullahkhan922@gmail.com>
2025-01-21 11:59:33 +0500

    webdriver commit

--- Wayloader.com Demo/mainclass.py
+++ Wayloader.com Demo/mainclass.py
@@ -1,9 +1,31 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")
 
-p1 = Person("John", 36)
+driver = webdriver.Chrome()
 
-print(p1.name)
-print(p1.age)
+try:
+    driver.get('https://12372.wayloader.com/login')
+
+
+    time.sleep(10)
+
+    username_field = driver.find_element(By.NAME, 'username')
+    username_field.send_keys('womowi9591@acroins.com')
+
+    password_field = driver.find_element(By.NAME, 'password')
+    password_field.send_keys('Test@1234')
+
+    login_button = driver.find_element(By.ID, 'login-button')
+    login_button.click()
+
+    time.sleep(5)
+    print("Login successful!")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    driver.quit()
