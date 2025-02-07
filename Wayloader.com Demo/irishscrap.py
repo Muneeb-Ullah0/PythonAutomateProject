f9cb7ace969f496a048a7102c50cc6fa9663e1bb
Muneeb Ullah khan <muneebullahkhan922@gmail.com>
2025-02-07 11:37:34 +0500

    new commit

---
+++ Wayloader.com Demo/irishscrap.py
@@ -0,0 +1,14 @@
+def student ():
+    name = "Muneeb Ullah"
+    age = 24
+    address = "gulberg green"
+
+    student_result = (name,age,address)
+    print(student_result)
+
+    if name == "Muneeb Ullah":
+        print("✅ yes")
+    else:
+        print("❌ No")
+
+student()
