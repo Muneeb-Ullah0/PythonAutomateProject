19d0dbe45d131b323a3b8505f97ead053663c4f6
Muneeb Ullah khan <muneebullahkhan922@gmail.com>
2025-02-19 22:53:37 +0500

    new commit

---
+++ Wayloader.com Demo/Tuples.py
@@ -0,0 +1,6 @@
+name = "muneeb"
+age = 34
+if name == "muneeb khan":
+     print("Name is muneeb")
+else:
+    print("name is not muneeb")
---
+++ Wayloader.com Demo/proof.py
@@ -0,0 +1,15 @@
+def Student ():
+    name = "Muneeb"
+    age = 23
+    address = " ghauri"
+    if name == "Muneeb" :
+        print("My name is : " + name )
+    else:
+        print("My name is Not" + name)
+
+    if age > 23 :
+        print("My age is :" )
+    else:
+        print("My age is Not " )
+
+Student()
