363f4b1931a21430add499a3177fe7ece0e3b338
Muneeb Ullah khan <muneebullahkhan922@gmail.com>
2025-05-06 16:29:08 +0500

    Selenium web automated

---
+++ Wayloader.com Demo/Task00.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.selenium.dev/documentation/webdriver/elements/locators/")
+
+time.sleep(5)
+
+element = driver.find_element(By.XPATH, "//h2[contains(text(),'Creating Locators')]")
+
+driver.execute_script("arguments[0].scrollIntoView();", element)
+
+driver.execute_script("arguments[0].style.border='3px solid blue'", element)
+
+time.sleep(5)
+driver.quit()
